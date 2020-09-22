cfedb75b0c5b1ff68cbec695877f3d7e3c3be490
Naoya323150403 <59727299+Naoya323150403@users.noreply.github.com>
2020-09-22 23:41:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from linebot.models import MessageEvent,TextMessage,TextSendMessage
 from linebot.exceptions import InvalidSignatureError
 
 #ぐるなび
-eat_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
-eat_key = "8281ecdda46d063ffea855429f6f080b"
+eat_url = "***************************"
+eat_key = "***************************"
 
 #LINE
-LINE_USER_ID = "Ua422def9c79c36afe76f39fae223771b"
-YOUR_CHANNEL_SECRET = "78b0cf60301eca8828eeb852fc946b1c"
-line_bot_api = LineBotApi("Hwvy0xyDokaKVJ48p5akpPK5xz0NfDgzk/LORYn1jfzs6XyNCWYZw4nvheMjpX3Y/hGGrXI7I+zqPNg0if32UPWokGfDuXxwuIpgaMY9fLA+0grpI13HIJUHLK5UDdtRaMDlPNQPxeBv/ruf8su3ggdB04t89/1O/w1cDnyilFU=")
+LINE_USER_ID = "***************************"
+YOUR_CHANNEL_SECRET = "***********************"
+line_bot_api = LineBotApi("***********************")
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 list_name = []
